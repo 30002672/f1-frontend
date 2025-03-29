688ff185e70b88f2063d6e196c2cf33c05f9a499
Alin Jain <90566914+30002672@users.noreply.github.com>
2025-03-29 00:24:52 -0400

    replaced backend url

--- app/page.tsx
+++ app/page.tsx
@@ -51,7 +51,7 @@ export default function Home() {
         Compound2: Number(formData.Compound2)
       };
 
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("https://f1-backend-dj4j.onrender.com:5000/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
